4201b8e53c89177c34833b774fe6fe66a63b04ce
Klemen Cede <klemen.cede@gmail.com>
2016-11-08 19:22:39 +0100

    Dodan meni za pavzo med igro

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject pauseMenu;
+
+    private Rigidbody rb;
+    private bool isPaused;
+    private GameObject[] pauseMenuChildren;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        isPaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused) {
+            pauseMenu.SetActive(true);
+            isPaused = true;
+            Time.timeScale = 0.0f;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && isPaused) {
+            pauseMenu.SetActive(false);
+            isPaused = false;
+            Time.timeScale = 1.0f;
+        }
+	}
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * 10);
+    }
+}
