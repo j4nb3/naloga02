a43dac49a7c9dc33815184485286bef3fc551d2d
katjar1337 <katja.rutnik@gmail.com>
2016-11-13 22:03:36 +0100

    Zvok in skripta za skrinjo
    Work in progress

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour {
+
+    bool isInRange;
+    bool itemExists = true;
+    public AudioClip sound;
+
+	void Start () {
+	    
+	}
+
+    void onTriggerEnter()
+    {
+        isInRange = true;
+    }
+
+    void onTriggerExit()
+    {
+        isInRange = false;
+    }
+
+    void OnGUI()
+    {
+        if(isInRange == true)
+        {
+            GUI.Label(new Rect(Screen.width / 2, Screen.width / 2, 100, 100), "Press [f] to search chest");
+        }
+    }
+
+    void Update()
+    {
+        if (isInRange == true)
+        {
+            if (Input.GetKeyDown("f"))
+            {
+                AudioSource.PlayClipAtPoint(sound, transform.position);
+                if(itemExists == true)
+                {
+                    GUI.Label(new Rect(Screen.width / 2, Screen.width / 2, 100, 100), "You found an item!");
+                    itemExists = false;
+                }
+                if(itemExists == false)
+                {
+                    GUI.Label(new Rect(Screen.width / 2, Screen.width / 2, 100, 100), "You already looted this chest.");
+                }
+                
+            }
+        }
+    }
+}
