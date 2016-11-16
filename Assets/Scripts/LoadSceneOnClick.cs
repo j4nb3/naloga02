a7233493b10a75fca6438141604b3390a4572045
katjar1337 <katja.rutnik@gmail.com>
2016-11-16 15:35:15 +0100

    mainScene = entryScene

--- Assets/Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -5,6 +5,6 @@ using UnityEngine.SceneManagement;
 public class LoadSceneOnClick : MonoBehaviour {
 
     public void LoadByIndex(int sceneIndex) {
-        SceneManager.LoadScene(sceneIndex);
+        Application.LoadLevel(sceneIndex);
     }
 }
