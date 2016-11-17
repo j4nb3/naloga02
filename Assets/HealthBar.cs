077c5d8ce8e8dd42e4606755fe59f4054a1a25af
j4nb3 <jan.bec@student.um.si>
2016-11-17 22:54:25 +0100

    Sedaj ni mogoče sprintati če je Stamina <=0

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using UnityStandardAssets.Characters.FirstPerson;
 using System.Collections;
 
+
 public class HealthBar : MonoBehaviour {
     public int max_HP = 500;
     public int max_ST = 300;
@@ -10,19 +11,26 @@ public class HealthBar : MonoBehaviour {
     public Slider Stamina;
     public Text HP;
     public Text ST;
-
+    FirstPersonController fpc;
+    float sprint, walk;
+    bool regenST = true;
     // Use this for initialization
     void Start () {
         Health.value = max_HP;
         Stamina.value = max_ST;
         InvokeRepeating("Heal", 5, 5); //poveča HP vsakih 5 sekund za +2
-        InvokeRepeating("Stam", 5, 5); //poveča ST vsakih 5 sekund za +2
+        InvokeRepeating("Stam", 3, 3); //poveča ST vsakih 3 sekund za +5
+        fpc = GameObject.FindObjectOfType<FirstPersonController>();
+        walk = (float)fpc.m_WalkSpeed;
+        sprint = (float)fpc.m_RunSpeed;
     }
 	
 	// Update is called once per frame
 	void Update () {
         HP.text = Health.value.ToString("0") + "/" + max_HP.ToString("0");
         ST.text = Stamina.value.ToString("0")+ "/" + max_ST.ToString("0");
+        fpc.m_RunSpeed = sprint;
+        regenST = true;
         if (Health.value <=0)
         {
             Dead();
@@ -31,11 +39,12 @@ public class HealthBar : MonoBehaviour {
 
         if (Input.GetKey("left shift"))
         {
+            regenST = false;
             Stamina.value -= 1;
         }
         if(Stamina.value<=0)
         {
-            //nemoreš sprintat
+            fpc.m_RunSpeed = walk;
         }
     }
     void Heal()
@@ -47,9 +56,9 @@ public class HealthBar : MonoBehaviour {
     }
     void Stam()
     {
-        if (Stamina.value != max_ST)
+        if (Stamina.value != max_ST && regenST==true)
         {
-            Stamina.value += 2;
+            Stamina.value += 5;
         }
     }
     void Dead()
