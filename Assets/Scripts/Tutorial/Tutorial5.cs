70acaa89066c4878bd213c1c2df606239be5630c
mandzokralju <rober.jan22@gmail.com>
2016-12-04 18:56:12 +0100

    Tutorial scene update 2

--- Assets/Scripts/Tutorial/Tutorial5.cs
+++ Assets/Scripts/Tutorial/Tutorial5.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class Tutorial5 : MonoBehaviour {
 
     public static string text = "You can defend yourself against monsters using weapons\nWeapons can be found all over maze in chests";
+    public static string text2 = "You can also pick up health and stamina orbs \n which will regenerate health and stamina";
 
     void OnTriggerEnter()
     {
         TutorialText.changeDisplayText(text, 2);
+        TutorialText.changeDisplayText(text2, 2);
     }
 }
