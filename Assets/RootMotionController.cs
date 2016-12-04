a50c9ffad5edb68459ee2af33104a018ab9db90c
Klemen Cede <klemen.cede@student.um.si>
2016-12-04 16:28:26 +0100

    Zombie, ki se premika + tab v prefab

---
+++ Assets/RootMotionController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class RootMotionController : MonoBehaviour {
+
+    void OnAnimatorMove() {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator) {
+            Vector3 newPosition = transform.position;
+            newPosition.z += animator.GetFloat("Walkspeed") * Time.deltaTime;
+            transform.position = newPosition;
+        }
+    }
+}
