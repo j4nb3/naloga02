0b42ca4a827e57773f44482d7acb6ff56db5bf59
j4nb3 <jan.bec@student.um.si>
2016-12-04 13:00:11 +0100

    Ammo bar, ammo crate ter reload

---
+++ Assets/Ammo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Ammo : MonoBehaviour {
+
+    public AudioSource source;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        source.Play();   
+        this.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/CrossScript.cs
+++ Assets/Scripts/CrossScript.cs
@@ -8,21 +8,79 @@ public class CrossScript : MonoBehaviour {
     public RawImage levo;
     public RawImage desno;
 
+    private int trenutno_noter;
+    private int trenutno_reload;
+    public Text noter;
+    public Text reload;
+
     // Use this for initialization
-    void Start () {
-	
-	}
+    void Start ()
+    {
+        trenutno_noter = 0;
+        trenutno_reload = 20;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        noter.text = "" + trenutno_noter;
+        reload.text = "" + trenutno_reload;
         if (Input.GetMouseButtonDown(0))
-        { 
-            gor.GetComponent<Animator>().enabled = true;
-            dol.GetComponent<Animator>().enabled = true;
-            levo.GetComponent<Animator>().enabled = true;
-            desno.GetComponent<Animator>().enabled = true;
-            Invoke("CakajAnim", 0.08f);
+        {
+            if(trenutno_noter > 0)
+            {
+                trenutno_noter--;
+                gor.GetComponent<Animator>().enabled = true;
+                dol.GetComponent<Animator>().enabled = true;
+                levo.GetComponent<Animator>().enabled = true;
+                desno.GetComponent<Animator>().enabled = true;
+                Invoke("CakajAnim", 0.08f);
+            }
+            else
+            {
+                if(trenutno_reload>0)
+                {
+                    if(trenutno_reload>24)
+                    {
+                        if (trenutno_noter == 0)
+                        {
+                            trenutno_noter += 24;
+                            trenutno_reload -= 24;
+                        }
+                        else
+                        {
+                            trenutno_reload = trenutno_reload - (24 - trenutno_noter);
+                            trenutno_noter = trenutno_noter + (24 - trenutno_noter);
+                        }
+                    }
+                    else if(trenutno_reload<24)
+                    {
+                        trenutno_noter += trenutno_reload;
+                        trenutno_reload = 0;
+                    }
+                }
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {          
+            if (trenutno_reload > 24)
+            {
+                if(trenutno_noter==0)
+                {
+                    trenutno_noter += 24;
+                    trenutno_reload -= 24;
+                }
+                else
+                {
+                    trenutno_reload = trenutno_reload - (24 - trenutno_noter);
+                    trenutno_noter = trenutno_noter + (24 - trenutno_noter);
+                }
+            }
+            else if (trenutno_reload < 24)
+            {
+                trenutno_noter += trenutno_reload;
+                trenutno_reload = 0;
+            }
         }
     }
     void CakajAnim()
@@ -35,5 +93,12 @@ public class CrossScript : MonoBehaviour {
         dol.transform.localPosition = new Vector3(0, -20, 0);
         levo.transform.localPosition = new Vector3(-20, 0, 0);
         desno.transform.localPosition = new Vector3(20, 0, 0);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ammo")
+        {
+            trenutno_reload += 10;
+        }
     }
 }
