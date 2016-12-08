40d42c1759c29c826203ef4b39960031599f7c3c
Klemen Cede <klemen.cede@student.um.si>
2016-12-08 17:15:28 +0100

    Implementiran pathfinding z A*

--- Assets/Scripts/AI/ZombieAI.cs
+++ Assets/Scripts/AI/ZombieAI.cs
@@ -6,6 +6,7 @@ using System.Collections;
 public class ZombieAI : MonoBehaviour {
 
     public UnityStandardAssets.Characters.FirstPerson.FirstPersonController player;
+    public Grid grid;
 
     private Transform m_transform;
     private float m_rotationSpeed = 3.00f;
@@ -18,8 +19,11 @@ public class ZombieAI : MonoBehaviour {
         Animator animator = GetComponent<Animator>();
 
         if (animator) {
-            m_transform.rotation = Quaternion.Slerp(m_transform.rotation, Quaternion.LookRotation(player.transform.position - m_transform.position - new Vector3(0,1,0)), m_rotationSpeed * Time.deltaTime);
-            m_transform.position += m_transform.forward * animator.GetFloat("Walkspeed") * Time.deltaTime;
+            //m_transform.rotation = Quaternion.Slerp(m_transform.rotation, Quaternion.LookRotation(player.transform.position - m_transform.position - new Vector3(0,1,0)), m_rotationSpeed * Time.deltaTime);
+            m_transform.rotation = Quaternion.Slerp(m_transform.rotation, Quaternion.LookRotation(grid.path[1].worldPosition - m_transform.position), m_rotationSpeed * Time.deltaTime);
+            Vector3 newPosition = m_transform.position + m_transform.forward * animator.GetFloat("Walkspeed") * Time.deltaTime;
+            newPosition.y = 0;
+            m_transform.position = newPosition;
             animator.SetFloat("DistanceToPlayer", Vector3.Distance(m_transform.position, player.transform.position));
         }
     }
