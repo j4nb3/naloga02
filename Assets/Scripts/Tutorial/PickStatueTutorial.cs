3fe08fac035e72a037c2d5a11db0ee0daa97c5b5
mandzokralju <rober.jan22@gmail.com>
2017-01-07 22:32:57 +0100

    "Dokončana" tutorial soba

---
+++ Assets/Scripts/Tutorial/PickStatueTutorial.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickStatueTutorial : MonoBehaviour {
+
+    private bool inRange = false;
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.F) && inRange == true)
+        {
+            Destroy(GameObject.Find("DragonStatue"));
+            Application.LoadLevel("MainScene");
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        TutorialText.changeDisplayText("Press F to pick up", 2);
+        inRange = true;
+    }
+
+}
